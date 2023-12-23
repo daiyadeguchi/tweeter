205f3b06bc88e31c76f4f755dcd40983aa9afe94
daiyadeguchi <double.d.rugby@gmail.com>
2023-12-23 20:37:01 +0900

    connected to postgres

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/daiyadeguchi/tweeter/db"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func main() {
+	db.ConnectPostgres()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, mfers!")
